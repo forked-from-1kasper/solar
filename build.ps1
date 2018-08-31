31f8d1295df9286e62b3135e526aa72ba885c69f
Siegmentation Fault <siegmentationfault@yandex.ru>
2018-08-31 14:53:18 +0700

    rewriting build.ps1

--- build.ps1
+++ build.ps1
@@ -1,6 +1,48 @@
 param (
-    [string] $Target = "build"
+    [string] $Target = "build",
+    [System.ConsoleColor] $CommandColor = [System.ConsoleColor]::Gray,
+    [System.ConsoleColor] $InfoColor = [System.ConsoleColor]::White
 )
+
+function New-Target() {
+    param (
+        [parameter(Mandatory=$true)][ScriptBlock] $Actions,
+        [parameter(Mandatory=$true)][string] $Comment,
+        [string[]] $Depends = @()
+    )
+    New-Object -TypeName PSObject -Property @{
+        'Actions' = $Actions;
+        'Comment' = $Comment;
+        'Depends' = $Depends
+    }
+}
+
+function PrintAndEvaluate() {
+    param (
+        [parameter(Mandatory=$true)][string] $Comment,
+        [parameter(Mandatory=$true)][ScriptBlock] $ScriptBlock
+    )
+    Write-Host "Info: $Comment" -ForegroundColor $InfoColor
+
+    $ExecutionContext.InvokeCommand.ExpandString(
+        $ScriptBlock.ToString()
+    ) -Split "`n" | % {
+        Write-Host -NoNewline '>'
+        Write-Host -ForegroundColor $CommandColor $_
+    }
+    & $ScriptBlock
+}
+
+function Build([string] $Target, $Targets) {
+    if (-Not $Targets.ContainsKey($Target)) {
+        Write-Error ('target "{0}" not found' -f $Target)
+        exit
+    }
+
+    $default = $Targets[$Target]
+    $default.Depends |% { Build $_ $Targets }
+    PrintAndEvaluate $default.Comment $default.Actions
+}
 
 $FSharpCompiler = "fsc"
 $FSharpParams = @("--nologo", "--nowarn:82")
@@ -20,56 +62,49 @@ $Files = @('improvements.fs'
            'keymap.fs')
 $Main = "main.fs"
 
-function PrintAndEvaluate() {
-    param (
-        [parameter(Mandatory=$true)][ScriptBlock] $ScriptBlock
-    )
-    $ExecutionContext.InvokeCommand.ExpandString(
-        ($ScriptBlock.ToString().Trim() -replace "`n", '' `
-                                        -replace '`', '' `
-                                        -replace '\s+', ' ')
-    ) | write-host -foregroundcolor Green
-    & $ScriptBlock
+$BuildExec = {
+    New-Item -Type Directory -Force $OutputDir | Out-Null
+    & $FSharpCompiler `
+        -r (Join-Path $OutputDir $OutputLibrary) `
+        $Main `
+        -o (Join-Path $OutputDir $OutputBinary) `
+        $FSharpParams
 }
 
-function Build($Target) {
-    switch ($Target) {
-        "build" {
-            Build "build-lib"
-            mkdir -f $OutputDir | out-null
-            PrintAndEvaluate {
-                & $FSharpCompiler `
-                    -r (join-path $OutputDir $OutputLibrary) `
-                    $Main `
-                    -o (join-path $OutputDir $OutputBinary) `
-                    $FSharpParams
-            }
-        }
-        "build-standalone" {
-            Build "build-lib"
-            PrintAndEvaluate {
-                & $FSharpCompiler `
-                    -r (join-path $OutputDir $OutputLibrary) `
-                    $Main `
-                    -o (join-path $OutputDir $OutputStandaloneBinary) `
-                    $FSharpParams --standalone
-            }
-        }
-        "build-lib" {
-            PrintAndEvaluate {
-                & $FSharpCompiler `
-                    $Files -a `
-                    -o (join-path $OutputDir $OutputLibrary) `
-                    $FSharpParams
-            }
-        }
-        "clean" {
-            rm -rec $OutputDir
-        }
-        default {
-            write-error ('unknown target "{0}"' -f $Target)
-        }
-    }
+$BuildStandalone = {
+    & $FSharpCompiler `
+        -r (Join-Path $OutputDir $OutputLibrary) `
+        $Main `
+        -o (Join-Path $OutputDir $OutputStandaloneBinary) `
+        $FSharpParams --standalone
 }
 
-Build $Target
+$BuildLib = {
+    & $FSharpCompiler `
+        $Files -a `
+        -o (Join-Path $OutputDir $OutputLibrary) `
+        $FSharpParams
+}
+
+$Clean = {
+    Remove-Item -Recurse $OutputDir
+}
+
+$Targets = @{
+    'build' =
+        New-Target -Actions $BuildExec `
+                   -Comment "building executable $OutputBinary" `
+                   -Depends @("build-lib");
+    'build-standalone' =
+        New-Target -Actions $BuildStandalone `
+                   -Comment "building standalone executable $OutputStandaloneBinary" `
+                   -Depends @("build-lib");
+    'build-lib' =
+        New-Target -Actions $BuildLib `
+                   -Comment "building library $OutputLibrary";
+    'clean' =
+        New-Target -Actions $Clean `
+                   -Comment "removing $OutputDir"
+}
+
+Build $Target $Targets
